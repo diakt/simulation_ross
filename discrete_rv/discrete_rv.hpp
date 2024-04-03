c7ed4b76d077bb615c981daaa37c281c27c47a1a
diakt <gavin.southerland@machinacognita.com>
2024-04-03 10:07:12 -0700

    unif_perm

--- discrete_rv/discrete_rv.hpp
+++ discrete_rv/discrete_rv.hpp
@@ -1,6 +1,5 @@
 // Chapter 4 mostly
 
-
 #ifndef DISCRETE_RV
 #define DISCRETE_RV
 #include <vector>
@@ -9,27 +8,38 @@
 
 #include "../rng/my_rng.hpp"
 
-std::vector<int> disc_inv_trans(std::vector<std::pair<int, float>> events, int seq_len)
-{
+std::vector<int> disc_inv_trans_seq(std::vector<std::pair<int, float>> events, int seq_len){
     float num;
     float curr;
     int pos;
 
     std::vector<int> res{};
-    for (int i = 0; i < seq_len; i++)
-    {   
+    for (int i = 0; i < seq_len; i++){
         num = my_rng();
         pos = 0;
         curr = 0.0;
-        std::cout << "rng: " << num << std::endl;
-        while (pos < events.size() && curr < num)
-        {
+        while (pos < events.size() && curr < num){
             curr += events[pos].second;
-            std::cout << i << " " << curr << std::endl;
             pos++;
         }
         res.push_back(pos - 1);
     }
+    return res;
+}
+
+std::vector<int> gen_unif_perm(int low, int high){
+    int len = high - low + 1;
+    std::vector<int> start{};
+    for (int i = low; i <= high; i++){
+        start.push_back(i);
+    }
+    std::vector<int> res{};
+    for (int i = 0; i < len; i++){
+        int curr = rand_int(0, len - i - 1);
+        res.push_back(start[curr]);
+        std::swap(start[curr], start[len - i - 1]);
+    }
+
     return res;
 }
 
--- rng/my_rng.hpp
+++ rng/my_rng.hpp
@@ -3,12 +3,20 @@
 
 #include <random>
 
+int rand_int(int a, int b){
+    if(a==b) return a;
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> distrib(a, b);
+    
+    return distrib(rng);
+}
 
 float my_rng(){
     std::random_device rd;
     std::mt19937 gen(rd());
-
     std::uniform_real_distribution<> distr(0.0, 1.0);
+
     float randomFloat = distr(gen);
     return randomFloat;
 }
